12c3c723e5bd56689297f82416934fa2e87c8a53
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-09-14 18:21:23 -0400

    changing database name

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "Alex-3891!",
-    database: "employees"
+    database: "burgers_db"
 });
 
 connection.connect(function (err) {
