866e4a43d70590aae0c67f92aab6801522a8b579
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-09-14 15:02:23 -0400

    Fixing schema, adding seed data

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES
+    (1, 'Cowboy Jack BBQ Burger', FALSE),
+    (2, 'Big Mac Burger', FALSE),
+    (3, 'Bacon Cheese Burger', FALSE),
+    (4, 'Veggie Burger', FALSE),
