4a3638ecb0e6fcda6ef50d1be71119476ad75edd
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-09-15 21:10:55 -0400

    Adding front end serve up files

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-const routes = require("");  // add route here
+const routes = require("./controllers/burgers_controller");
 
 app.use(routes);
 
