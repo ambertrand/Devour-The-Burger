5728b52027dc821ae22fbf623491ddb00e594853
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-09-19 16:42:46 -0400

    Adding Jaws DB connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 const mysql = require("mysql");
+let connection;
 
 
 // Accessing the database
-let connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 });
+};
+
 
 connection.connect(function(err) {
     if (err) {
@@ -18,4 +24,5 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
   });
 
+connection.connect();
 module.exports = connection;
