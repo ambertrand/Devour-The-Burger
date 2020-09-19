d5b13232b02265a66d7be323d239995dec4373f3
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-09-19 17:55:09 -0400

    Addding env back in

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 3306,
-    user: "root",
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 });
